b4c24f589c89e288c28f3b2cd6fda5222c740916
Aleix <nigeon@gmail.com>
2019-01-15 13:45:00 +0100

    Added censusUrl in smart contracts

--- contracts/VotingEntity.sol
+++ contracts/VotingEntity.sol
@@ -5,6 +5,7 @@ contract VotingEntity {
     struct Entity {
         bool exists;
         string name;
+        string censusRequestUrl;
     }
 
     mapping (address => Entity) public entities;
@@ -13,15 +14,18 @@ contract VotingEntity {
     constructor() public {
     }
 
-    function createEntity(string memory _name) public {
+    function createEntity(string memory _name, string memory _url) public {
         require(entities[msg.sender].exists == false, "Entity already exists");
 
-        entities[msg.sender] = Entity(true, _name);
+        entities[msg.sender] = Entity(true, _name, _url);
         entitiesIndex.push(msg.sender);
     }
 
-    function getEntity(address _address) public view returns(string memory name, bool exists){
-        return (entities[_address].name, entities[_address].exists);
+    function getEntity(address _address) 
+        public view 
+        returns(string memory name, bool exists, string memory censusRequestUrl){
+
+        return (entities[_address].name, entities[_address].exists, entities[_address].censusRequestUrl);
     }
 
     function getEntitiesLength() public view
--- test/entity.js
+++ test/entity.js
@@ -8,12 +8,13 @@ contract('VotingEntity', function (accounts) {
     });
 
     const entityInput = {
-        name: "The Entity Name"
+        name: "The Entity Name",
+        censusRequestUrl: "http://vocdoni.io/requestCenus"
     };
 
     it("Creates a new entity and checks its data", async () => {
         let instance = await VotingEntity.deployed();
-        await instance.createEntity(entityInput.name, { from: accounts[0] });
+        await instance.createEntity(entityInput.name, entityInput.censusRequestUrl, { from: accounts[0] });
         
         let entitiesLength = await instance.getEntitiesLength();
         assert.equal(entitiesLength.valueOf(), 1, "Only one entity exists");
@@ -24,13 +25,14 @@ contract('VotingEntity', function (accounts) {
         let entity = await instance.getEntity(accounts[0]);
 
         assert.equal(entityInput.name, entity.name, "The name should match the input");
+        assert.equal(entityInput.censusRequestUrl, entity.censusRequestUrl, "The census URL should match the input");
     });
 
     it("Can't create/override the same entity again", async () => {
         let instance = await VotingEntity.deployed();
 
         try {
-            await instance.createEntity(entityInput.name, { from: accounts[0] });
+            await instance.createEntity(entityInput.name, entityInput.censusRequestUrl, { from: accounts[0] });
         }catch(_error){
             error = _error
         }
