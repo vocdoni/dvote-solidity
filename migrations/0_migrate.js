99da2175038f90a4e7021e317fc63b7618d44fac
Jør∂¡ <itsbrickpop@gmail.com>
2019-01-30 13:58:35 +0100

    Printing the deployment address used on testing

--- migrations/0_migrate.js
+++ migrations/0_migrate.js
@@ -1,7 +1,8 @@
 var VotingProcess = artifacts.require("VotingProcess");
 var VotingEntity = artifacts.require("VotingEntity");
 
-module.exports = function(deployer) {
+module.exports = function (deployer, networkName, accounts) {
+  console.log("Deploying from", accounts[0]);
   // deployment steps
   deployer.deploy(VotingProcess);
   deployer.deploy(VotingEntity);
