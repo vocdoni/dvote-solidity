47fee1fd1914938b790867331cdfa94847be2afa
Xavi <xavings@gmail.com>
2019-03-30 09:35:37 +0000

    Updating migration script to point to EntityResolver Contract

--- migrations/0_migrate.js
+++ migrations/0_migrate.js
@@ -1,9 +1,9 @@
 var VotingProcess = artifacts.require("VotingProcess");
-var VotingEntity = artifacts.require("VotingEntity");
+var EntityResolver = artifacts.require("EntityResolver");
 
 module.exports = function (deployer, networkName, accounts) {
   console.log("Deploying from", accounts[0]);
   // deployment steps
   deployer.deploy(VotingProcess);
-  deployer.deploy(VotingEntity);
+  deployer.deploy(EntityResolver);
 };
