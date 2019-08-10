4c7ae7a52d5a233ab773ea1b5d8715c004334aea
Xavi <xavings@gmail.com>
2019-08-10 18:32:37 +0200

    New contract, compiling...

--- contracts/VotingProcess.sol
+++ contracts/VotingProcess.sol
@@ -1,61 +1,51 @@
 pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
 
 contract VotingProcess {
 
     // DATA AND STRUCTS
 
+    address contractOwner;
+
     struct Process {
-        address entityResolver;    // A pointer to the Entity's resolver instance to fetch metadata
         address entityAddress;     // The address of the Entity's creator
-        string processName;
-        string metadataContentUri; // Content URI to fetch the JSON metadata from
-        uint256 startTime;         // block.timestamp after which votes can be registered
-        uint256 endTime;           // block.timestamp after which votes can be registered
-        string voteEncryptionPublicKey;
+        string processMetadataHash; // Content URI to fetch the JSON metadata from
         string voteEncryptionPrivateKey;  // Key revealed after the vote ends so that scrutiny can start
         bool canceled;
+        string resultsHash;                   // N vote batches registered
+    }
 
-        address[] relayList;       // Relay addresses to let users fetch the Relay data
-        mapping (address => Relay) relays;       // Mapping relayAddress => Relay
-        
-        mapping (uint64 => string) voteBatches;  // Mapping from [0..N-1] to Content URI's to fetch the vote batches
-        uint64 voteBatchCount;                   // N vote batches registered
-    }
-    
-    struct Relay {
-        bool active;
-        string publicKey;
-        string messagingUri;
-    }
-    
+    string[] validators;
+    string[] oracles;
+    string version;
+    string genesis;
+    uint chainId;
+
     // MAPPINGS
 
-    mapping (bytes32 => Process) public processes;         // processId => process data
+    Process[] public processes;                 // Array of Process struct
+    mapping (bytes32 => uint) processesIndex;   // Mapping of processIds with processess idx
+    
     mapping (address => uint) public entityProcessCount;   // index of the last process for a given address
 
     // EVENTS
 
     event ProcessCreated(address indexed entityAddress, bytes32 processId);
     event ProcessCanceled(address indexed entityAddress, bytes32 processId);  // entityAddress could be removed. Keeping for web3 testability issues
-    event RelayAdded(bytes32 indexed processId, address relayAddress);
-    event RelayDisabled(bytes32 indexed processId, address relayAddress);
-    event BatchRegistered(bytes32 indexed processId, uint64 batchNumber);
+    event ValidatorAdded( string validatorPublicKey);
+    event ValidatorDisabled(string validatorPublicKey);
     event PrivateKeyRevealed(bytes32 indexed processId, string privateKey);
 
     // MODIFIERS
 
     modifier onlyEntity(bytes32 processId) {
-        require(processes[processId].entityAddress == msg.sender, "Invalid entity");
+        uint processIndex = getProcessIndex(processId);
+        require(processes[processIndex].entityAddress == msg.sender, "Invalid entity");
     	_;
     }
 
-    modifier onlyActiveRelay(bytes32 processId) {
-        require(isActiveRelay(processId, msg.sender), "Invalid relay");
-    	_;
-    }
-
-    modifier onlyActiveProcessId(bytes32 processId) {
-        require(!processes[processId].canceled, "Canceled process");
+    modifier onlyContractOwner() {
+        require(msg.sender == contractOwner, "Only contract owner");
     	_;
     }
 
@@ -67,140 +57,101 @@ contract VotingProcess {
         return getProcessId(entityAddress, idx);
     }
     // Compute a process ID
-    function getProcessId(address entityAddress, uint processIndex) public pure returns (bytes32){
-        return keccak256(abi.encodePacked(entityAddress, processIndex));
+    function getProcessId(
+        address entityAddress,
+        uint processCountIndex
+        ) public view returns (bytes32){
+        return keccak256(abi.encodePacked(entityAddress, processCountIndex, genesis, chainId));
     }
-    
+
+    function getProcessIndex(
+        bytes32 processId
+        ) public view returns (uint){
+        return processesIndex[processId];
+    }
+
     // METHODS
 
     function create(
-    	address entityResolver,
-    	string memory processName,
-    	string memory metadataContentUri,
-    	uint256 startTime,
-    	uint256 endTime,
-    	string memory voteEncryptionPublicKey
+    	string memory processMetadataHash
     ) public {
-        require(startTime > block.timestamp, "Invalid startTime");
-        require(endTime > startTime, "Invalid endTime");
-        require(bytes(processName).length > 0, "Empty processName");
-        require(bytes(metadataContentUri).length > 0, "Empty metadataContentUri");
-        require(bytes(voteEncryptionPublicKey).length > 0, "Empty voteEncryptionPublicKey");
+
+        require(bytes(processMetadataHash).length > 0, "Empty processMetadataHash");
 
         address entityAddress = msg.sender;
         bytes32 processId = getNextProcessId(entityAddress);
+        require(processesIndex[processId]==0, "ProcessId already exists");
 
-        processes[processId].entityResolver = entityResolver;
-        processes[processId].entityAddress = entityAddress;
-        processes[processId].processName = processName;
-        processes[processId].metadataContentUri = metadataContentUri;
-        processes[processId].startTime = startTime;
-        processes[processId].endTime = endTime;
-        processes[processId].voteEncryptionPublicKey = voteEncryptionPublicKey;
+        Process memory process = Process({
+            entityAddress:entityAddress,
+            processMetadataHash:processMetadataHash,
+            voteEncryptionPrivateKey:"",
+            canceled:false,
+            resultsHash:""
+            });
 
+        processes.push(process);
+        processesIndex[processId] = processes.length-1;
         entityProcessCount[entityAddress]++;
-        
+
         emit ProcessCreated(entityAddress, processId);            
     }
 
     function get(bytes32 processId) public view returns (
-    	address entityResolver,
     	address entityAddress,
-    	string memory processName,
-    	string memory metadataContentUri,
-    	uint256 startTime,
-    	uint256 endTime,
-    	string memory voteEncryptionPublicKey,
+    	string memory processMetadataHash,
+    	string memory voteEncryptionPrivateKey,
         bool canceled
     ) {
-        entityResolver = processes[processId].entityResolver;
-        entityAddress = processes[processId].entityAddress;
-        processName = processes[processId].processName;
-        metadataContentUri = processes[processId].metadataContentUri;
-        startTime = processes[processId].startTime;
-        endTime = processes[processId].endTime;
-        voteEncryptionPublicKey = processes[processId].voteEncryptionPublicKey;
-        canceled = processes[processId].canceled;
+        uint processIndex = processesIndex[processId];
+        entityAddress = processes[processIndex].entityAddress;
+        processMetadataHash = processes[processIndex].processMetadataHash;
+        voteEncryptionPrivateKey = processes[processIndex].voteEncryptionPrivateKey;
+        canceled = processes[processIndex].canceled;
     }
-    
-    function cancel(bytes32 processId) public onlyEntity(processId) onlyActiveProcessId(processId) {
-        require(processes[processId].startTime > block.timestamp, "Process started");
 
-        processes[processId].relayList.length = 0;
-        processes[processId].canceled = true;
+    function cancel(bytes32 processId) public onlyEntity(processId) {
+        uint processIndex = getProcessIndex(processId);
+        processes[processIndex].canceled = true;
         emit ProcessCanceled(msg.sender, processId);
     }
-    
-    function addRelay(bytes32 processId, address relayAddress, string memory publicKey, string memory messagingUri) public onlyEntity(processId) onlyActiveProcessId(processId) {
-        require(!processes[processId].relays[relayAddress].active, "Relay already active");
 
-        processes[processId].relayList.push(relayAddress);
-        processes[processId].relays[relayAddress].publicKey = publicKey;
-        processes[processId].relays[relayAddress].messagingUri = messagingUri;
-        processes[processId].relays[relayAddress].active = true;
+    function addValidator(string memory validatorPublicKey) public onlyContractOwner()  {
 
-        emit RelayAdded(processId, relayAddress);
+        validators.push(validatorPublicKey);
+        emit ValidatorAdded(validatorPublicKey);
+    }
+
+
+    function stringsAreEqual(string memory str1, string memory str2) public pure returns(bool)
+    {
+        return (keccak256(abi.encodePacked((str1))) == keccak256(abi.encodePacked((str2))) );
+    }
+
+    function removeValidator(uint idx, string memory validatorPublicKey) public onlyContractOwner() {
+
+        require(stringsAreEqual(validators[idx], validatorPublicKey), "Validator to remove does not match index");
+        // swap with the last element from the list
+        validators[idx] = validators[validators.length - 1];
+        validators.length--;
+
+        emit ValidatorDisabled( validatorPublicKey);
+    }
+
+    function getValidators() public view returns (string[] memory) {
+        return validators;
     }
     
-    function disableRelay(bytes32 processId, address relayAddress) public onlyEntity(processId) onlyActiveProcessId(processId) {
-        require(isActiveRelay(processId, relayAddress), "Relay already disabled");
-
-        uint len = processes[processId].relayList.length;
-        for (uint i = 0; i < len; i++) {
-            if(processes[processId].relayList[i] != relayAddress) continue;
-
-            // swap with the last element from the list
-            processes[processId].relayList[i] = processes[processId].relayList[len - 1];
-            processes[processId].relayList.length--;
-            break;
-        }
-
-        processes[processId].relays[relayAddress].active = false;
-        emit RelayDisabled(processId, relayAddress);
-    }
-    
-    function getRelayIndex(bytes32 processId) public view returns (address[] memory) {
-        return processes[processId].relayList;
-    }
-    
-    function isActiveRelay(bytes32 processId, address relayAddress) public view returns (bool) {
-        return processes[processId].relays[relayAddress].active;
-    }
-    
-    function getRelay(bytes32 processId, address relayAddress) public view returns (string memory publicKey, string memory messagingUri) {
-        publicKey = processes[processId].relays[relayAddress].publicKey;
-        messagingUri = processes[processId].relays[relayAddress].messagingUri;
-    }
-    
-    function registerVoteBatch(bytes32 processId, string memory dataContentUri) public onlyActiveRelay(processId) onlyActiveProcessId(processId) {
-        require(block.timestamp >= processes[processId].startTime, "Process not started");
-        require(block.timestamp < processes[processId].endTime, "Process ended");
-
-        processes[processId].voteBatches[processes[processId].voteBatchCount] = dataContentUri;
-
-        emit BatchRegistered(processId, processes[processId].voteBatchCount);
-
-        processes[processId].voteBatchCount++;
-    }
-    
-    function getVoteBatchCount(bytes32 processId) public view returns (uint64) {
-        return processes[processId].voteBatchCount;
-    }
-    
-    function getBatch(bytes32 processId, uint64 batchNumber) public view returns (string memory batchContentUri) {
-        return processes[processId].voteBatches[batchNumber];
-    }
-    
-    function revealPrivateKey(bytes32 processId, string memory privateKey) public onlyEntity(processId) onlyActiveProcessId(processId) {
-        require(block.timestamp >= processes[processId].endTime, "Process not ended");
-
-        processes[processId].voteEncryptionPrivateKey = privateKey;
+    function revealPrivateKey(bytes32 processId, string memory privateKey) public onlyEntity(processId) {
+        uint processIndex = getProcessIndex(processId);
+        processes[processIndex].voteEncryptionPrivateKey = privateKey;
 
         emit PrivateKeyRevealed(processId, privateKey);
     }
 
     function getPrivateKey(bytes32 processId) public view returns (string memory privateKey) {
-        privateKey = processes[processId].voteEncryptionPrivateKey;
+        uint processIndex = getProcessIndex(processId);
+        privateKey = processes[processIndex].voteEncryptionPrivateKey;
     }
 
 }
